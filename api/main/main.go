36bac43dd0bcb2db268bad958f8b4849d4db4501
Irina Kobzeva <Kotariffy@yandex.ru>
2024-06-03 01:06:34 +0300

    Updated some colors.

--- api/main/main.go
+++ api/main/main.go
@@ -1,15 +1,63 @@
-package main
+// package main
 
-import (
-	"net/http"
-)
+// import (
+// 	"fmt"
+// 	"html/template"
+// 	"log"
+// 	"net/http"
+// )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "index.html")
-}
+// var apiRequestDuration = prometheus.NewHistogramVec(
+// 	prometheus.HistogramOpts{
+// 		Name:    "api_request_duration_seconds",
+// 		Help:    "Histogram for the request duration of the public API, partitioned by status class.",
+// 		Buckets: prometheus.ExponentialBuckets(0.1, 1.5, 5),
+// 	},
+// 	[]string{"status_class"},
+// )
 
-func main() {
-	http.HandleFunc("/", index)
+// func handler_homepage(w http.ResponseWriter, r *http.Request) {
 
-	http.ListenAndServe(":80", nil)
-}
+// 	tmpl := template.Must(template.ParseFiles("../../index.html"))
+// 	tmpl.Execute(w, nil)
+
+// //указываем путь к нужному файлу
+// path := filepath.Join("../../index.html")
+// //создаем html-шаблон
+// tmp, err := template.ParseFiles(path)
+// if err != nil {
+// 	http.Error(w, err.Error(), 400)
+// 	return
+// }
+// //выводим шаблон клиенту в браузер
+// err = tmp.Execute(w, nil)
+// if err != nil {
+// 	http.Error(w, err.Error(), 400)
+// 	return
+// }
+// ================================================================= //
+// status := http.StatusOK
+
+// timer := prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
+// 	switch {
+// 	case status >= 500: // Server Error
+// 		apiRequestDuration.WithLabelValues("5xx").Observe(v)
+// 	case status >= 400: // Client Error
+// 		apiRequestDuration.WithLabelValues("4xx").Observe(v)
+// 	case status >= 300: // Redirect
+// 		apiRequestDuration.WithLabelValues("3xx").Observe(v)
+// 	case status >= 200: // OK
+// 		apiRequestDuration.WithLabelValues("2xx").Observe(v)
+// 	case status >= 100: // INFO
+// 		apiRequestDuration.WithLabelValues("1xx").Observe(v)
+
+// 	}
+// }))
+// defer timer.ObserveDuration()
+
+// }
+
+// func main() {
+// 	http.HandleFunc("/", handler_homepage)
+// 	log.Fatal(http.ListenAndServe(":8000", nil))
+// }
